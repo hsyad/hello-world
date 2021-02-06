a1b2b283199cdfa8b28fc46961e15b348e0c6aca
Nurul Husna Binti Ahmad Radzuan <husnaboxer27@gmail.com>
2021-02-06 12:45:37 +0800

    ni yang aku try tambah

--- helloWorld.java
+++ helloWorld.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class HelloWorld {
+public class helloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Ni yang aku try tambah, jadi tak HAHAHAHA(husna)");
     }
 
 }
